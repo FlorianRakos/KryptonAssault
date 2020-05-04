49248654ed743d3ffd525449959d57cbe78501fe
Florian167 <florian.rakos@yahoo.com>
2020-05-04 19:18:49 +0200

    Script Changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,24 +7,37 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject explosionFX;
     [SerializeField] GameObject smokeFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void AddNonTriggerBoxCollider()
     {
-        
+     Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
     }
+
+    
 
     private void OnParticleCollision(GameObject other)
     {
+        GameObject fxE =  Instantiate(explosionFX, transform.position, Quaternion.identity);
+        fxE.transform.parent = parent;
+        GameObject fxS = Instantiate(smokeFX, transform.position, Quaternion.identity);
+        fxS.transform.parent = parent;
 
-        //explosionFX.SetActive(true);
-        //smokeFX.SetActive(true);
-        print(gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
+
         Destroy (gameObject);
 
 
